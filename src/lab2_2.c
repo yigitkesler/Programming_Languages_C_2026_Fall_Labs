a1b7e05c06206bc3377bb7e26db3048a4f6e3e5e
yigitkesler <yigitkesler@hotmail.com>
2026-09-26 18:32:55 +0000

    Complete Lab 2 tasks

--- src/lab2_2.c
+++ src/lab2_2.c
@@ -12,17 +12,26 @@
 */
 
 long long factorial(int n) {
-    // TODO: compute factorial iteratively
-    return 1; // placeholder
+  long long result = 1;
+
+  for (int i = 1; i <= n; i++) {
+    result = result * i;
+  }
+
+  return result;
 }
 
 int main(void) {
-    int n;
+  int n;
 
-    printf("Enter a non-negative integer n: ");
-    scanf("%d", &n);
+  printf("Enter a non-negative integer n: ");
+  scanf("%d", &n);
 
-    // TODO: validate input, call function, print result
-
-    return 0;
+  if (n < 0) {
+    printf("Error: n must be non-negative.\n");
+  } else {
+    long long result = factorial(n);
+    printf("%d! = %lld\n", n, result);
+  }
+  return 0;
 }
